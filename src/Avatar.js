fd4cc46568172b296dd4eae567ebeccdd3910315
arsalan1990 <52230752+arsalan1990@users.noreply.github.com>
2020-06-04 22:22:27 +0500

    Update Avatar.js

--- src/Avatar.js
+++ src/Avatar.js
@@ -1,47 +1,62 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './style.css';
 import 'tachyons';
 import list from './list';
 
-const Avatar = (props) => {
+class Avatar extends Component{
 
-    const avatarlist = [
-        {
-            id:1,
-            name: "Alvin",
-            work: "Web Developer"
-        },
-        {
-            id:2,
-            name: "Johnny",
-            work: "Front End"
-        },
-        {
-            id:3,
-            name: "Rocky",
-            work: "Back End"
-        },
-        {
-            id:4,
-            name: "Codi",
-            work: "Data Base"
+    constructor(){
+        super();
+        this.state={
+            name: "welcome to Avatar World "
         }
-    ]
+    }
 
-   const avatar_card =  avatarlist.map((avatarcard, i) =>{
-        return <list id={avatarlist[i].name}
-                    name={avatarlist[i].name}
-                    work={avatarlist[i].work}/>
-    })
-    return ( 
-    <div>
-        <h1> Welcome to Avatar World </h1>
+    namechange(){
+        this.setState({
+            name: "Subscribe"
+        })
+    }
+        render() {
+            const avatarlist = [
+                {
+                    id:1,
+                    name: "Alvin",
+                    work: "Web Developer"
+                },
+                {
+                    id:2,
+                    name: "Johnny",
+                    work: "Front End"
+                },
+                {
+                    id:3,
+                    name: "Rocky",
+                    work: "Back End"
+                },
+                {
+                    id:4,
+                    name: "Codi",
+                    work: "Data Base"
+                }
+            ]
+        
+           const avatar_card =  avatarlist.map((avatarcard, i) => {
+                return <list key={i} id={avatarlist[i].name}
+                            name={avatarlist[i].name}
+                            work={avatarlist[i].work}/>
+            })
+            return ( 
+                <div className="mainpage">
+                    <h1> {this.state.name} </h1>
+            
+                    {avatar_card}
+            
+                <button onClick={()=> this.namechange()}>Subscribe</button> 
+                </div> 
+                )
+        }
+    }
 
-        {avatar_card}
-
-    <button>Subscribe</button> 
-    </div> 
-    )
-}
 
 export default Avatar;
