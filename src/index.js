933ef515cc6a71e8c4b18e9053a87d935fa8a031
arsalan1990 <52230752+arsalan1990@users.noreply.github.com>
2020-06-04 13:38:15 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import ReactDOM from 'react-dom';
 function Hi() {
   return <div>Hello World!</div>;
 }
-  function greet() {
-    return "Hi Dave";
+  function Hi(props) {
+  return <div>Hello {props.name}!</div>;
   }
 
-ReactDOM.render(<Hi/>,document.querySelector('#root'));
+ReactDOM.render(<Hi name="Dave"/>,document.querySelector('#root'));
 
 
 
