3a4ca66e0c0cef1e97c69dc6a12cf970cd31e018
arsalan1990 <52230752+arsalan1990@users.noreply.github.com>
2020-06-04 13:53:44 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 
 const element = <h1>Hello World</h1>;
 
-ReactDOM.render(element, document.getElementById('#root'));
+ReactDOM.render(element, document.getElementById('root'));
 
 
 
