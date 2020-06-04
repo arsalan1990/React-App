53b75725decab5eccb3c370578b9f21dc806209b
arsalan1990 <52230752+arsalan1990@users.noreply.github.com>
2020-06-04 17:25:04 +0500

    Update Avatar.js

--- src/Avatar.js
+++ src/Avatar.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Avatar = () => {
     return (
-    <div>
-        <img src="https://joeschmoe.io/api/v1/arsalan" alt="Avatar" />
+    <div class = "avatar_style">
+        <img src="https://joeschmoe.io/api/v1/alvin alt="Avatar" />
     <h1> Janny </h1>
     <p> Web Developer</p>
     </div>
