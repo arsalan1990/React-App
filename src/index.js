41b9991be13a1263836a1743275c43e18713401e
arsalan1990 <52230752+arsalan1990@users.noreply.github.com>
2020-06-04 15:12:19 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App'
+import Demo from './Component'
 import registerServiceWorker from './registerServiceWorker';
 
 
-const element = <h1>Hello React</h1>;
+const element = <h1>Hello World</h1>;
 
 ReactDOM.render(element, document.getElementById('root'));
 
