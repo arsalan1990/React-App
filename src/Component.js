4fd375252ed6370ca12c3ed2a10c45f11412992c
arsalan1990 <52230752+arsalan1990@users.noreply.github.com>
2020-06-04 16:36:29 +0500

    Update Component.js

--- src/Component.js
+++ src/Component.js
@@ -4,7 +4,7 @@ import React,{Component} from 'react';
 class Demo extends Component{
     render(){
         return <div>
-         <h1>Hello world</h1>
+         <h1>Hello {this.props.name}</h1>
         <p> Welcome to my channel</p>
         <p> This is my new tv channel.</p>
         </div>
