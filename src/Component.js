fca4757b14d32e13a090677bf317230063a70b5e
arsalan1990 <52230752+arsalan1990@users.noreply.github.com>
2020-06-04 17:00:30 +0500

    Update Component.js

--- src/Component.js
+++ src/Component.js
@@ -1,9 +1,9 @@
-import React,{Component} from 'react';
+import React from 'react';
 import './style.css'
 
 const Demo = (props) => {
     return <div class="main_style">
-    <h1>Hello {props.name}    </h1>
+    <h1>Hello {props.name}</h1>
    <p> Welcome to my channel</p>
    <p> This is my new tv channel.</p>
    </div>
