e1b1ef55fa9e4a74e74a89a4f4daf2d88d63c966
arsalan1990 <52230752+arsalan1990@users.noreply.github.com>
2020-06-04 16:28:26 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App'
 import Demo from './Component'
 import * as serviceWorker from './serviceWorker';
 
