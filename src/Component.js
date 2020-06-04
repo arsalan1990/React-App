65c52bd9fea94b0ebf03de4088b793b6a925de8e
arsalan1990 <52230752+arsalan1990@users.noreply.github.com>
2020-06-04 16:43:53 +0500

    Update Component.js

--- src/Component.js
+++ src/Component.js
@@ -1,10 +1,10 @@
 import React,{Component} from 'react';
-
+import './style.css'
 
 class Demo extends Component{
     render(){
-        return <div>
-         <h1>Hello {this.props.name}</h1>
+        return <div class="main_style">
+         <h1>Hello {this.props.name}    </h1>
         <p> Welcome to my channel</p>
         <p> This is my new tv channel.</p>
         </div>
