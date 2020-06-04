22b2c3632c91ada47dfded747c82782143aef840
arsalan1990 <52230752+arsalan1990@users.noreply.github.com>
2020-06-04 15:12:13 +0500

    Create Component.js

---
+++ src/Component.js
@@ -0,0 +1,12 @@
+import React,{Component} from 'react';
+import ReactDom from 'react-dom';
+
+class Demo extends Component{
+    render(){
+        return <div>
+         <h1>Hello world</h1>
+        <p> Welcome to my channel</p>
+        </div>
+    }
+}
+export default Demo;
