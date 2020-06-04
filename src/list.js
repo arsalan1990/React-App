23bf4a85f9989ed173953430f1411ad9712e53c2
arsalan1990 <52230752+arsalan1990@users.noreply.github.com>
2020-06-04 20:25:29 +0500

    Create list.js

---
+++ src/list.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const list = (props) => {
+    return(
+    <div class = "avatar_style ma4 bg-light-purple dib pa4 tc">
+        <img src="https://joeschmoe.io/api/v1/alvin" alt="Avatar" />
+    <h1 className=""> {props.name} </h1>
+    <p> {props.work}</p>
+    </div>
+    )
+}
+
+export default list;
