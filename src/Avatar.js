7a931b1262194ba519ee04f7860641e20b6a6aa4
arsalan1990 <52230752+arsalan1990@users.noreply.github.com>
2020-06-04 20:48:55 +0500

    Update Avatar.js

--- src/Avatar.js
+++ src/Avatar.js
@@ -4,13 +4,44 @@ import 'tachyons';
 import list from './list'
 
 const Avatar = (props) => {
+
+    const avatarlist = [
+        {
+            id=1,
+            name = "Alvin",
+            work = "Web Developer"
+        },
+        {
+            id=2,
+            name = "Johnny",
+            work = "Front End"
+        },
+        {
+            id=1,
+            name = "Rocky",
+            work = "Back End"
+        },
+        {
+            id=1,
+            name = "Codi",
+            work = "Data Base"
+        }
+    ]
     return ( <div>
         <h1> Welcome to Avatar World </h1>
 
-    <list id="1" name="Alvin" work="Web Developer"/>
-    <list id="1" name="Johnny" work="Front End"/>
-    <list id="1" name="Rocky" work="Back End"/>
-    <list id="1" name="Codi" work="Designer"/>
+    <list id={avatarlist[0].name}
+            name={avatarlist[0].name}
+            work={avatarlist[0].name}/>
+    <list id={avatarlist[1].name}
+            name={avatarlist[1].name}
+            work={avatarlist[1].name}/>
+    <list id={avatarlist[2].name}
+            name={avatarlist[2].name}
+            work={avatarlist[2].name}/>
+    <list id={avatarlist[3].name}
+            name={avatarlist[3].name}
+            work={avatarlist[3].name}/>
 
     <button>Subscribe</button> </div>   
     )
